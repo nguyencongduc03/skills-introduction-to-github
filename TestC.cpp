6ff7495d29daa89990b430fe72bfe0ba1d9fd6b3
Nguyen Cong Duc <nguyencongduc@MacBook-Air-cua-Nguyen.local>
2025-03-24 22:18:07 +0700

    duc dep trai

---
+++ TestC.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    if (x < y) {
+        cout << "x is less than y\n";
+    }
+    else if (x > y) {
+        cout << "x is greater than y\n";
+    }
+    else {
+        cout << "x is equal to y\n";
+    }
+    return 0;
+}
