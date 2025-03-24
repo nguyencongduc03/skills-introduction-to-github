5d5140027bc1527523ca918365f20c56ea2e0833
Nguyen Cong Duc <nguyencongduc@MacBook-Air-cua-Nguyen.local>
2025-03-24 23:37:56 +0700

    lam lai lan 2

--- TestC.cpp
+++ TestC.cpp
@@ -2,16 +2,5 @@
 using namespace std;
 
 int main() {
-    int x,y;
-    cin >> x >> y;
-    if (x < y) {
-        cout << "x is less than y\n";
-    }
-    else if (x > y) {
-        cout << "x is greater than y\n";
-    }
-    else {
-        cout << "x is equal to y\n";
-    }
-    return 0;
+    cout<<"Hello world\n";
 }
